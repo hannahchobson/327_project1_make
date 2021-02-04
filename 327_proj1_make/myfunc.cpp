f1789437730e0be0b7e1044e6ac63994e93d3a00
hannahchobson <71147573+hannahchobson@users.noreply.github.com>
2021-02-04 18:19:05 -0500

    Add files via upload

---
+++ 327_proj1_make/myfunc.cpp
@@ -0,0 +1,15 @@
+/*
+ * myfunc.cpp
+ *
+ *  Created on: Feb 4, 2021
+ *      Author: Hannah Hobson
+ */
+
+#include<iostream>
+#include <string>
+#include "myfunc.h"
+
+std::string func(){
+	return "hello";
+}
+
---
+++ 327_proj1_make/myfunc.h
@@ -0,0 +1,11 @@
+/*
+ * myfunc.h
+ *
+ *  Created on: Feb 4, 2021
+ *      Author: Hannah Hobson
+ */
+
+#include <iostream>
+#include <string>
+
+std::string func();
